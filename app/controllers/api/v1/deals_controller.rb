61fba02ee66055956e558be19e1003767b03af1c
Yuta Fujii <y.f.mar2@gmail.com>
2018-12-20 13:01:05 +0900

    change deals controller api mode

--- app/controllers/api/v1/deals_controller.rb
+++ app/controllers/api/v1/deals_controller.rb
@@ -1,4 +1,4 @@
-class DealsController < ApplicationController
+class Api::V1::DealsController < Api::V1::BaseController
   def index
     @deals = Deal.all
     render json: @deals
--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,34 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'deals#index'
 
-  resources :issuers
-  resources :investors
-  resources :user_issuers, only: [:edit, :update, :destroy]
-  resources :user_deals, only: [:edit, :update, :destroy]
-  resources :user_investors, only: [:index, :edit, :update, :destroy]
-  resources :treasuries
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      root to: 'deals#index'
 
-  resources :deals do
-    resources :tranches, only: [:index, :create] do
-      resources :marketings, only: [:index, :show, :new, :create] do
-        resources :feedbacks, only: [:index, :show, :new, :create] do
-          resources :orders, only: [:new, :create]
+      resources :issuers
+      resources :investors
+      resources :user_issuers, only: [:edit, :update, :destroy]
+      resources :user_deals, only: [:edit, :update, :destroy]
+      resources :user_investors, only: [:index, :edit, :update, :destroy]
+      resources :treasuries
+
+      resources :deals do
+        resources :tranches, only: [:index, :create] do
+          resources :marketings, only: [:index, :show, :new, :create] do
+            resources :feedbacks, only: [:index, :show, :new, :create] do
+              resources :orders, only: [:new, :create]
+            end
+          end
+          resources :very_important_investors, only: [:index, :new, :create]
         end
       end
-      resources :very_important_investors, only: [:index, :new, :create]
+
+      resource :trahches, only: [:edit, :update, :destroy]
+      resource :marketings, only: [:edit, :update, :destroy]
+      resource :feedbacks, only: [:edit, :update, :destroy]
+      resource :very_important_investors, only: [:destroy]
     end
   end
-
-  resource :trahches, only: [:edit, :update, :destroy]
-  resource :marketings, only: [:edit, :update, :destroy]
-  resource :feedbacks, only: [:edit, :update, :destroy]
-  resource :very_important_investors, only: [:destroy]
 
 
   # issuers
