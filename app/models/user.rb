0355aa3f0a1fc6c59ad262aff808a2d0471c260e
Yuta Fujii <y.f.mar2@gmail.com>
2018-12-23 03:01:45 +0900

    add issuer and investor

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  ROLE = {
+    "Origination": 0,
+    "Syndication": 1,
+    "Sales": 2
+  }
+
+  validates :role, inclusion: {in: ROLE.values}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,10 @@
 if Rails.env.development?
   puts "Deleting User instances"
   User.destroy_all
-  puts "Deleting Issuer instances"
-  Issuer.destroy_all
-  puts "Deleting Investor instances"
-  Investor.destroy_all
+  # puts "Deleting Issuer instances"
+  # Issuer.destroy_all
+  # puts "Deleting Investor instances"
+  # Investor.destroy_all
   puts "Deleting Deal instances"
   Deal.destroy_all
 end
@@ -77,19 +77,68 @@ if SmallInvestorCategory.all.empty?
 end
 
 # Create users <origination>
-
-
 # Create users <syndicate>
-
-
 # Create users <sales>
-
+User::ROLE.each do |key, value|
+  puts "Create User #{key}"
+  10.times do
+    gimei = Gimei.name
+    email_head = gimei.first.romaji + gimei.last.romaji
+    attributes = {
+      email: Faker::Internet.email(email_head),
+      password: "123456",
+      first_name: gimei.first.kanji,
+      last_name: gimei.last.kanji,
+      tel: "03-1234-" + Faker::PhoneNumber.subscriber_number,
+      role: value
+    }
+    instance = User.create(attributes)
+  end
+  puts "Finished"
+end
 
 # Create issuers
+if Issuer.all.empty?
+  puts "Create Issuer"
+  csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
+  file_path = File.expand_path("../../lib/assets/stocklist.csv", __FILE__)
 
+  CSV.foreach(file_path, csv_options) do |row|
+    name_read = row["銘柄名"]
+    # this is temporary code
+    small_code = SmallCode.find_by(name: row["業種分類"])
+    unless Issuer.find_by(name: name_read)
+      instance = Issuer.new(name: name_read)
+      instance.small_code = small_code
+      instance.save!
+    end
+  end
+  puts "Finished"
+end
 
 # Create investors
+if Investor.all.empty?
+  puts "Create investor"
+    csv_options = { col_sep: '.', quote_char: '"', headers: :first_row}
+    file_path = File.expand_path("../../lib/assets/qib.csv", __FILE__)
 
+    CSV.foreach(file_path, csv_options) do |row|
+      name_read = row["商号等"]
+      small_investor_category = SmallInvestorCategory.all.sample
+      address = Gimei.address
+
+      attributes = {
+        name: name_read,
+        team: "Credit",
+        small_investor_category_id: small_investor_category.id,
+        address: address.kanji,
+        tel: "03-8888-" + Faker::PhoneNumber.subscriber_number,
+        email: Faker::Internet.email
+      }
+
+    end
+  puts "Finished"
+end
 
 # Create deals
 # Create tranches
