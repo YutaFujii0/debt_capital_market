7fec88ce40467379f11b0c8d55f33999cf7bc385
Yuta Fujii <y.f.mar2@gmail.com>
2019-01-05 11:15:20 +0900

    add issuers and deal category

--- app/controllers/api/v1/deals_controller.rb
+++ app/controllers/api/v1/deals_controller.rb
@@ -2,12 +2,17 @@ class Api::V1::DealsController < Api::V1::BaseController
   def index
     @deals = Deal.all
     # @tranches = @deals.first.tranches
-    render json: @deals.to_json(include: { tranches:
-      { include: { marketings:
-        { include: { feedbacks:
-          { include: :orders }
-        }}
-      }}
-    })
+    render json: @deals.to_json(
+      include: [
+        tranches:
+          { include: { marketings:
+            { include: { feedbacks:
+              { include: :orders }
+            }}
+          }},
+        issuer: { only: :name },
+        deal_category: {  }
+      ]
+    )
   end
 end
