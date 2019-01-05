b7bf2f1b433d9628a480950ea5c186e34d1e1d0c
Yuta Fujii <y.f.mar2@gmail.com>
2019-01-05 12:56:52 +0900

    Connect has many relations about user_x table

--- app/controllers/api/v1/deals_controller.rb
+++ app/controllers/api/v1/deals_controller.rb
@@ -2,17 +2,26 @@ class Api::V1::DealsController < Api::V1::BaseController
   def index
     @deals = Deal.all
     # @tranches = @deals.first.tranches
-    render json: @deals.to_json(
-      include: [
-        tranches:
-          { include: { marketings:
-            { include: { feedbacks:
-              { include: :orders }
-            }}
-          }},
-        issuer: { only: :name },
-        deal_category: {  }
-      ]
-    )
+    render json: {
+      deals: @deals.to_json(
+        include: [
+          tranches:
+            { include: [
+              marketings:
+                { include: { feedbacks:
+                  { include: [
+                    :investor,
+                    :orders
+                  ] }
+                }},
+              treasury: {}
+            ] },
+          issuer: { only: :name },
+          deal_category: { only: :name }
+        ]
+      ),
+      investors: Investor.all.to_json
+    }
+
   end
 end
